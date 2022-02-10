3538ec0b6333f250a41ae4bbcf09966d4abc3e0d
Andrei Shumailov <gentoo1993@gmail.com>
2022-02-10 20:47:53 +0100

    Create an empty teamplate Spring Boot project

---
+++ src/main/kotlin/com/sourcejet/sourcejet/SourcejetApplication.kt
@@ -0,0 +1,11 @@
+package com.sourcejet.sourcejet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SourcejetApplication
+
+fun main(args: Array<String>) {
+	runApplication<SourcejetApplication>(*args)
+}
---
+++ src/test/kotlin/com/sourcejet/sourcejet/SourcejetApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sourcejet.sourcejet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SourcejetApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
